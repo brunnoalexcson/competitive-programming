0d1ca0762f7bc287e14adf3249169d034b358dee
Brunno Alexcson <brunnoalexcsontech@gmail.com>
2026-04-25 23:42:52 -0300

    Adding code 3495

---
+++ beecrowd/1-beginner/3495-gianzin-berrateiro.cpp
@@ -0,0 +1,21 @@
+/*
+ID: 3495
+Name: Gianzin Berranteiro
+Platform: Beecrowd
+Link: https://judge.beecrowd.com/en/problems/view/3495
+Date: 2026-04-25
+Language: C++
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    cout << "o numero eh sobrinho" << endl;
+
+    return 0;
+}
